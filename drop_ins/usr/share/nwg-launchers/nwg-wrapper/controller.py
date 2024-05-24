ac469968f1d4b5d54286d625a23fe3016e043dca
d[-_-]b <dragonkeeper@nixheads.co.uk>
2024-05-24 17:37:07 +0100

    Add files via upload

---
+++ drop_ins/usr/share/nwg-launchers/nwg-wrapper/controller.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def main():
+    print("#img path='/usr/share/nwg-launchers/nwg-wrapper/DeckBG.png' width=1280 height=800 align=center")
+
+
+
+if __name__ == '__main__':
+    main()
